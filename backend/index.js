b8b50478fa051d071dd14b6d6efc7e7752dfb165
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-09 22:42:29 +0200

    Fix(backend): refact hard coded urls and ports for production

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
 
 const studentsRouter = require("./routes/students");
 
 const app = express();
 
+const port = process.env.PORT || 8000;
+const dbURI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/students_protal_mean";
+
 mongoose
-  .connect("mongodb://localhost:27017/students_protal_mean", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("connected to mongodb");
-    app.listen(8000, (err) => {
+    app.listen(port, (err) => {
       if (err) return console.log(err);
 
       return console.log("started server");
@@ -23,6 +26,7 @@ mongoose
     console.log(err);
   });
 
+app.use(helmet());
 app.use(express.json());
 app.use(cors());
 app.use("/students", studentsRouter);
